9777feb6796a6ab1e12a572851f7f0f05ee5c9af
dongxinyi <dongxinyi@172.20.10.2>
2021-10-17 20:45:08 +0800

    authorization for user

--- src/main/java/sg/com/studymama/controller/PostsController.java
+++ src/main/java/sg/com/studymama/controller/PostsController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -32,6 +33,7 @@ import sg.com.studymama.service.RecommendationService;
 import sg.com.studymama.service.SpringDataPostService;
 @CrossOrigin(origins = "*")
 @RestController
+@Secured("ROLE_USER")
 public class PostsController {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(PostsController.class);
